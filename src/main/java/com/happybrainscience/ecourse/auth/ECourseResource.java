4d94f7b581921b2894ccf4eddc05e93e614746d7
Timothy Heider <timothy.heider@gmail.com>
2022-07-03 09:15:00 -0700

    video lesson URL

--- src/main/java/com/happybrainscience/ecourse/auth/ECourseResource.java
+++ src/main/java/com/happybrainscience/ecourse/auth/ECourseResource.java
@@ -41,14 +41,14 @@ public class ECourseResource {
     }    
     
     private static final String[] VIDEO_FILENAMES = {
-        "lesson_1_working_happiness.mp4",
-        "lesson_2_choose_happiness.mp4",
-        "lesson_3_soothe_stress.mp4",
-        "lesson_4_practice_positivity.mp4",
-        "lesson_5_flow_to_goals.mp4",
-        "lesson_6_minimize_multi-tasking.mp4",
-        "lesson_7_prioritize_people.mp4",
-        "action_plan_-_thrive_from_nine_to_five.mp4"
+        "lesson_1_working_happiness",
+        "lesson_2_choose_happiness",
+        "lesson_3_soothe_stress",
+        "lesson_4_practice_positivity",
+        "lesson_5_flow_to_goals",
+        "lesson_6_minimize_multi-tasking",
+        "lesson_7_prioritize_people",
+        "action_plan_-_thrive_from_nine_to_five"
     };
 
     @Produces({MediaType.APPLICATION_JSON})
