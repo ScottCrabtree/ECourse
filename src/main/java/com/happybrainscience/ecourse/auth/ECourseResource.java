fdac4b01d4f71f450bf4a53971a29c891067c16a
Timothy Heider <timothy.heider@gmail.com>
2022-07-12 08:54:53 -0700

    google signon

--- src/main/java/com/happybrainscience/ecourse/auth/ECourseResource.java
+++ src/main/java/com/happybrainscience/ecourse/auth/ECourseResource.java
@@ -1,17 +1,27 @@
 package com.happybrainscience.ecourse.auth;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.happybrainscience.ecourse.application.CaptionTextResource;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import com.happybrainscience.ecourse.application.ProductVersion;
 import com.happybrainscience.ecourse.application.TextResourceController;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import org.apache.log4j.Logger;
 
 /**
@@ -54,12 +64,35 @@ public class ECourseResource {
         "action_plan_-_thrive_from_nine_to_five"
     };
 
+    private static final String GOOGLE_OAUTH_CLIENT_ID = "886115731938-6la64nljk0av0bguht0vqpdnjt5n3hjc.apps.googleusercontent.com";
+    
     @Produces({MediaType.APPLICATION_JSON})
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Path("/credential")
     @POST
     public void postCredentials(@FormParam("credential") String credential, @FormParam("g_csrf_token") String token) {
-        LOGGER.debug("google auth credential " + credential + " token " + token);
+        try {
+            LOGGER.debug("google auth credential " + credential + " token " + token);
+            GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), JacksonFactory.getDefaultInstance())
+                    .setAudience(Arrays.asList(GOOGLE_OAUTH_CLIENT_ID))
+                    .build();
+            GoogleIdToken googleToken = verifier.verify(token);
+            if (googleToken == null) {
+                throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+            } else {
+                if (verifier.verify(googleToken)) {
+                    LOGGER.debug("token is valid " + googleToken.toString());
+                    String emailAddress = googleToken.getPayload().getEmail();
+                    LOGGER.debug("resolved Google account " + emailAddress);
+                } else {
+                    LOGGER.error("invalid token");
+                    throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+                }
+            }
+        } catch (GeneralSecurityException | IOException ex) {
+            LOGGER.error("Google auth failure", ex);
+            throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+        }        
     }
     
     @Produces({MediaType.APPLICATION_JSON})
